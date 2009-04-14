edf004261426ae1ac3ac71e2c792839ee813cc4f
Dominic Sydler <sydler@puzzle.ch>
2009-04-14 11:54:46 +0000

    Overview-Form set as standard, onleave-functionality implemented

--- src/gui/main_window.cc
+++ src/gui/main_window.cc
@@ -1,6 +1,6 @@
 /*
 #| sepiola - Open Source Online Backup Client
-#| Copyright (C) 2007, 2008  stepping stone GmbH
+#| Copyright (C) 2007, 2008 stepping stone GmbH
 #|
 #| This program is free software; you can redistribute it and/or
 #| modify it under the terms of the GNU General Public License
@@ -8,12 +8,12 @@
 #|
 #| This program is distributed in the hope that it will be useful,
 #| but WITHOUT ANY WARRANTY; without even the implied warranty of
-#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #| GNU General Public License for more details.
 #|
 #| You should have received a copy of the GNU General Public License
 #| along with this program; if not, write to the Free Software
-#| Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#| Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
 #include <QDebug>
@@ -24,46 +24,32 @@
 #include "gui/main_window.hh"
 #include "gui/password_dialog.hh"
 
-MainWindow::MainWindow ( MainModel *model ) : QMainWindow()
+MainWindow::MainWindow( MainModel *model ) : QMainWindow()
 {
-	setupUi ( this );
+	setupUi( this );
 	Settings* settings = Settings::getInstance();
 	resize( settings->getWindowSize() );
 	move( settings->getWindowPosition() );
-	this->setWindowTitle(settings->getApplicationName());
+	this->setWindowTitle( settings->getApplicationName() );
 	this->model = model;
 
-	QObject::connect( this->model, SIGNAL( showInformationMessageBox( QString ) ),
-						this, SLOT( showInformationMessageBox( QString ) ) );
-	QObject::connect( this->model, SIGNAL( showCriticalMessageBox( QString ) ),
-						this, SLOT( showCriticalMessageBox( QString ) ) );
-	QObject::connect( this->model, SIGNAL( askForServerPassword( const QString&, bool, int*, const QString& ) ),
-						this, SLOT( showServerPasswordDialog( const QString&, bool, int*, const QString& ) ) );
-	QObject::connect( this->model, SIGNAL( askForClientPassword( const QString&, bool, int*, const QString& ) ),
-						this, SLOT( showClientPasswordDialog( const QString&, bool, int*, const QString& ) ) );
-	QObject::connect( this->model, SIGNAL( showProgressDialog( const QString& ) ),
-						this, SLOT( showProgressDialog( const QString& ) ) );
-	QObject::connect( this->model, SIGNAL( appendInfoMessage( const QString& ) ),
-						this, SLOT( appendInfoMessage( const QString& ) ) );
-	QObject::connect( this->model, SIGNAL( appendErrorMessage( const QString& ) ),
-						this, SLOT( appendErrorMessage( const QString& ) ) );
-	QObject::connect( this->model, SIGNAL( finishProgressDialog() ),
-						this, SLOT( finishProgressDialog() ) );
-	QObject::connect( this->model, SIGNAL( closeProgressDialog() ),
-						this, SLOT( closeProgressDialog() ) );
+	QObject::connect( this->model, SIGNAL( showInformationMessageBox( QString ) ), this, SLOT( showInformationMessageBox( QString ) ) );
+	QObject::connect( this->model, SIGNAL( showCriticalMessageBox( QString ) ), this, SLOT( showCriticalMessageBox( QString ) ) );
+	QObject::connect( this->model, SIGNAL( askForServerPassword( const QString&, bool, int*, const QString& ) ), this, SLOT( showServerPasswordDialog( const QString&, bool, int*, const QString& ) ) );
+	QObject::connect( this->model, SIGNAL( askForClientPassword( const QString&, bool, int*, const QString& ) ), this, SLOT( showClientPasswordDialog( const QString&, bool, int*, const QString& ) ) );
+	QObject::connect( this->model, SIGNAL( showProgressDialog( const QString& ) ), this, SLOT( showProgressDialog( const QString& ) ) );
+	QObject::connect( this->model, SIGNAL( appendInfoMessage( const QString& ) ), this, SLOT( appendInfoMessage( const QString& ) ) );
+	QObject::connect( this->model, SIGNAL( appendErrorMessage( const QString& ) ), this, SLOT( appendErrorMessage( const QString& ) ) );
+	QObject::connect( this->model, SIGNAL( finishProgressDialog() ), this, SLOT( finishProgressDialog() ) );
+	QObject::connect( this->model, SIGNAL( closeProgressDialog() ), this, SLOT( closeProgressDialog() ) );
 
 	// log file signals/slots
-	QObject::connect( this->model, SIGNAL( appendInfoMessage( const QString& ) ),
-							this, SIGNAL( writeLog( const QString& ) ) );
-	QObject::connect( this->model, SIGNAL( appendErrorMessage( const QString& ) ),
-							this, SIGNAL( writeLog( const QString& ) ) );
+	QObject::connect( this->model, SIGNAL( appendInfoMessage( const QString& ) ), this, SIGNAL( writeLog( const QString& ) ) );
+	QObject::connect( this->model, SIGNAL( appendErrorMessage( const QString& ) ), this, SIGNAL( writeLog( const QString& ) ) );
 
 	if ( settings->isReinstalled() )
 	{
-		int answer = QMessageBox::question ( this, tr ( "Keep settings?" ),
-																				 tr ( "The application has been reinstalled.\n"
-																							"Do you want to keep the settings?" ),
-																				 QMessageBox::Yes | QMessageBox::No );
+		int answer = QMessageBox::question( this, tr( "Keep settings?" ), tr( "The application has been reinstalled.\n", "Do you want to keep the settings?" ), QMessageBox::Yes | QMessageBox::No );
 		switch ( answer )
 		{
 			case QMessageBox::Yes:
@@ -77,18 +63,20 @@ MainWindow::MainWindow ( MainModel *model ) : QMainWindow()
 				break;
 		}
 	}
-	this->backupForm = new BackupForm ( 0, this->model );
-	this->restoreForm = new RestoreForm ( 0, this->model );
-	this->settingsForm = new SettingsForm ( 0, this->model );
+	this->overviewForm = new OverviewForm( 0, this->model );
+	this->backupForm = new BackupForm( 0, this->model );
+	this->restoreForm = new RestoreForm( 0, this->model );
+	this->settingsForm = new SettingsForm( 0, this->model );
 	this->logfileForm = new LogfileForm( 0, this->model );
 
 	stackedLayout = new QStackedLayout( frameMain );
+	stackedLayout->addWidget( overviewForm );
 	stackedLayout->addWidget( backupForm );
 	stackedLayout->addWidget( restoreForm );
 	stackedLayout->addWidget( settingsForm );
 	stackedLayout->addWidget( logfileForm );
-	frameMain->setLayout ( stackedLayout );
-	MainWindow::setFormIndex( SETTINGS );
+	frameMain->setLayout( stackedLayout );
+	MainWindow::setFormIndex( OVERVIEW );
 }
 
 MainWindow::~MainWindow()
@@ -99,42 +87,34 @@ void MainWindow::showServerPasswordDialog( const QString& username, bool isUsern
 {
 	Settings* settings = Settings::getInstance();
 	PasswordDialog passwordDialog( username, isUsernameEditable );
-	QObject::connect( &passwordDialog, SIGNAL( abort() ),
-						this->model, SLOT( abortLogin() ) );
-	QObject::connect( &passwordDialog, SIGNAL( processPasswordReturnValues( const QString&, const QString&, bool ) ),
-						settings, SLOT( setServerUserNameAndPassword( const QString&, const QString&, bool ) ) );
-	if (msg != "")
-		passwordDialog.setDialogMessage(msg);
-    int dialog_result = passwordDialog.exec(); 
-	if (result) 
+	QObject::connect( &passwordDialog, SIGNAL( abort() ), this->model, SLOT( abortLogin() ) );
+	QObject::connect( &passwordDialog, SIGNAL( processPasswordReturnValues( const QString&, const QString&, bool ) ), settings, SLOT( setServerUserNameAndPassword( const QString&, const QString&, bool ) ) );
+	if ( msg != "" )
+		passwordDialog.setDialogMessage( msg );
+	int dialog_result = passwordDialog.exec();
+	if ( result )
 	{
-		*result = dialog_result; 
+		*result = dialog_result;
 	}
-	QObject::disconnect( &passwordDialog, SIGNAL( abort() ),
-							this->model, SIGNAL( abortLogin() ) );
-	QObject::disconnect( &passwordDialog, SIGNAL( processPasswordReturnValues( const QString&, const QString&, bool ) ),
-							settings, SLOT( setServerUserNameAndPassword( const QString&, const QString&, bool ) ) );
+	QObject::disconnect( &passwordDialog, SIGNAL( abort() ), this->model, SIGNAL( abortLogin() ) );
+	QObject::disconnect( &passwordDialog, SIGNAL( processPasswordReturnValues( const QString&, const QString&, bool ) ), settings, SLOT( setServerUserNameAndPassword( const QString&, const QString&, bool ) ) );
 }
 
 void MainWindow::showClientPasswordDialog( const QString& username, bool isUsernameEditable, int* result, const QString& msg )
 {
 	Settings* settings = Settings::getInstance();
 	PasswordDialog passwordDialog( username, isUsernameEditable );
-	QObject::connect( &passwordDialog, SIGNAL( abort() ),
-						this->model, SLOT( abortLogin() ) );
-	QObject::connect( &passwordDialog, SIGNAL( processPasswordReturnValues( const QString&, const QString&, bool ) ),
-						settings, SLOT( setClientUserNameAndPassword( const QString&, const QString&, bool ) ) );
-	if (msg != "")
-		passwordDialog.setDialogMessage(msg);
-    int dialog_result = passwordDialog.exec(); 
-	if (result) 
+	QObject::connect( &passwordDialog, SIGNAL( abort() ), this->model, SLOT( abortLogin() ) );
+	QObject::connect( &passwordDialog, SIGNAL( processPasswordReturnValues( const QString&, const QString&, bool ) ), settings, SLOT( setClientUserNameAndPassword( const QString&, const QString&, bool ) ) );
+	if ( msg != "" )
+		passwordDialog.setDialogMessage( msg );
+	int dialog_result = passwordDialog.exec();
+	if ( result )
 	{
-		*result = dialog_result; 
+		*result = dialog_result;
 	}
-	QObject::disconnect( &passwordDialog, SIGNAL( abort() ),
-							this->model, SIGNAL( abortLogin() ) );
-	QObject::disconnect( &passwordDialog, SIGNAL( processPasswordReturnValues( const QString&, const QString&, bool ) ),
-							settings, SLOT( setClientUserNameAndPassword( const QString&, const QString&, bool ) ) );
+	QObject::disconnect( &passwordDialog, SIGNAL( abort() ), this->model, SIGNAL( abortLogin() ) );
+	QObject::disconnect( &passwordDialog, SIGNAL( processPasswordReturnValues( const QString&, const QString&, bool ) ), settings, SLOT( setClientUserNameAndPassword( const QString&, const QString&, bool ) ) );
 }
 
 void MainWindow::showInformationMessageBox( const QString& message )
@@ -152,10 +132,11 @@ void MainWindow::showCriticalMessageBox( const QString& message )
 void MainWindow::setFormIndex( FORM_INDEX index )
 {
 	qDebug() << "MainWindow::setFormIndex(" << index << ")";
-	switch( index )
+	switch ( index )
 	{
 		case SETTINGS:
-			if (this->stackedLayout->currentIndex() != SETTINGS) {
+			if ( this->stackedLayout->currentIndex() != SETTINGS )
+			{
 				this->settingsForm->reload();
 			}
 			break;
@@ -163,10 +144,20 @@ void MainWindow::setFormIndex( FORM_INDEX index )
 			// do nothing
 			break;
 	}
-	if (this->stackedLayout->currentIndex() == SETTINGS) {
-		((SettingsForm*)this->stackedLayout->currentWidget())->onLeave();
+	bool formChangeOk = true;
+	if ( this->stackedLayout->currentIndex() == SETTINGS )
+	{
+		formChangeOk = (( SettingsForm* )this->stackedLayout->currentWidget() )->onLeave();
 	}
-	this->stackedLayout->setCurrentIndex( index );
+	if ( formChangeOk )
+	{
+		this->stackedLayout->setCurrentIndex( index );
+	}
+}
+
+void MainWindow::on_btnOverview_clicked()
+{
+	showOverviewForm();
 }
 
 void MainWindow::on_btnBackup_clicked()
@@ -189,6 +180,11 @@ void MainWindow::on_btnLogfile_clicked()
 	showLogfileForm();
 }
 
+void MainWindow::on_actionOverview_triggered()
+{
+	showOverviewForm();
+}
+
 void MainWindow::on_actionBackup_triggered()
 {
 	showBackupForm();
@@ -209,15 +205,20 @@ void MainWindow::on_actionLogfile_triggered()
 	showLogfileForm();
 }
 
+void MainWindow::showOverviewForm()
+{
+	MainWindow::setFormIndex( OVERVIEW );
+}
+
 void MainWindow::showBackupForm()
 {
-	MainWindow::setFormIndex ( BACKUP );
+	MainWindow::setFormIndex( BACKUP );
 }
 
 void MainWindow::showRestoreForm()
 {
-	MainWindow::setFormIndex ( RESTORE );
-	if( !this->restoreForm->isInitialized() )
+	MainWindow::setFormIndex( RESTORE );
+	if ( !this->restoreForm->isInitialized() )
 	{
 		this->restoreForm->initPrefixes();
 	}
@@ -225,7 +226,7 @@ void MainWindow::showRestoreForm()
 
 void MainWindow::showSettingsForm()
 {
-	MainWindow::setFormIndex ( SETTINGS );
+	MainWindow::setFormIndex( SETTINGS );
 }
 
 void MainWindow::showLogfileForm()
@@ -242,7 +243,7 @@ void MainWindow::on_actionAbout_triggered()
 	aboutDialog.exec();
 }
 
-void MainWindow::closeEvent ( QCloseEvent *event )
+void MainWindow::closeEvent( QCloseEvent *event )
 {
 	Settings* settings = Settings::getInstance();
 	settings->saveWindowSize( size() );
@@ -252,11 +253,10 @@ void MainWindow::closeEvent ( QCloseEvent *event )
 	event->accept();
 }
 
-void MainWindow::showProgressDialog( const QString&  title )
+void MainWindow::showProgressDialog( const QString& title )
 {
 	outputDialog = new OutputDialog( title );
-	QObject::connect( outputDialog, SIGNAL( abort() ),
-						this->model, SLOT( abortProcessSlot() ) );
+	QObject::connect( outputDialog, SIGNAL( abort() ), this->model, SLOT( abortProcessSlot() ) );
 	outputDialog->show();
 	QApplication::processEvents();
 }
@@ -279,7 +279,7 @@ void MainWindow::finishProgressDialog()
 
 void MainWindow::closeProgressDialog()
 {
-	outputDialog->done(0);
+	outputDialog->done( 0 );
 	QApplication::processEvents();
 }
 
