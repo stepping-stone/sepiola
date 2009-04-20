cc83af64b78e78a6775d2dbd1b7f9f054b701923
Dominic Sydler <sydler@puzzle.ch>
2009-04-20 04:51:42 +0000

    read lastBackups from applicationDataSettings

--- src/gui/overview_form.cc
+++ src/gui/overview_form.cc
@@ -28,16 +28,17 @@
 #include "settings/settings.hh"
 #include "utils/debug_timer.hh"
 #include "model/scheduled_task.hh"
+#include "model/backup_task.hh"
 
-OverviewForm::OverviewForm(QWidget *parent, MainModel *model) : QWidget(parent)
+OverviewForm::OverviewForm( QWidget *parent, MainModel *model ) : QWidget( parent )
 {
-	COLOR_BLACK = qRgba(0, 0, 0, 255);
-	COLOR_WHITE = qRgba(255, 255, 255, 255);
-	COLOR_BACKUP = qRgba(255, 0, 0, 255);
-	COLOR_SNAPSHOT = qRgba(255, 127, 0, 255);
-	COLOR_FREE = qRgba(0, 191, 0, 255);
+	COLOR_BLACK = qRgba( 0, 0, 0, 255 );
+	COLOR_WHITE = qRgba( 255, 255, 255, 255 );
+	COLOR_BACKUP = qRgba( 255, 0, 0, 255 );
+	COLOR_SNAPSHOT = qRgba( 255, 175, 0, 255 );
+	COLOR_FREE = qRgba( 0, 191, 0, 255 );
 
-	setupUi(this);
+	setupUi( this );
 	initialized = false;
 
 	this->model = model;
@@ -45,18 +46,18 @@ OverviewForm::OverviewForm(QWidget *parent, MainModel *model) : QWidget(parent)
 
 	// build some data for testing purposes only
 	QSet<ScheduledTask::WeekdaysEnum> wd;
-	wd.insert(ScheduledTask::MONDAY);
-	wd.insert(ScheduledTask::TUESDAY);
-	wd.insert(ScheduledTask::TUESDAY); // and once more for testing
-	wd.insert(ScheduledTask::FRIDAY);
-	myTask.setType(ScheduledTask::AT_WEEKDAYS_AND_TIME);
-	myTask.setWeekdays(wd);
-	myTask.setTimeToRun(QTime::currentTime());
+	wd.insert( ScheduledTask::MONDAY );
+	wd.insert( ScheduledTask::TUESDAY );
+	wd.insert( ScheduledTask::TUESDAY ); // and once more for testing
+	wd.insert( ScheduledTask::FRIDAY );
+	myTask.setType( ScheduledTask::AT_WEEKDAYS_AND_TIME );
+	myTask.setWeekdays( wd );
+	myTask.setTimeToRun( QTime::currentTime() );
 	qDebug() << myTask.toString();
 
 	// refresh form
 	this->refreshSpaceStatistic();
-	this->refreshLastBackupsOverview(3);
+	this->refreshLastBackupsOverview();
 
 }
 
@@ -68,42 +69,52 @@ void OverviewForm::on_btnBackupNow_pressed()
 {
 }
 
-void OverviewForm::refreshLastBackupsOverview(int n)
+void OverviewForm::refreshLastBackupsOverview()
 {
+	Settings* settings = Settings::getInstance();
 	QStringList fieldnames_status;
 	fieldnames_status << "labelIconStatusLastBackup_%1" << "labelStatusLastBackup_%1" << "labelDateLastBackup_%1";
 	QImage img;
 
 	// fill map with a pixmap for each status
-	QMap<ScheduledTask::StatusEnum, QPixmap> status_pixmap;
-	img.load(":/main/sign_ok.svg");
-	status_pixmap.insert(ScheduledTask::OK, QPixmap::fromImage(img));
-	img.load(":/main/sign_error.svg");
-	status_pixmap.insert(ScheduledTask::ERROR, QPixmap::fromImage(img));
-	img.load(":/main/sign_warning.svg");
-	status_pixmap.insert(ScheduledTask::WARNING, QPixmap::fromImage(img));
+	QMap<BackupTask::StatusEnum, QPixmap> status_pixmap;
+	img.load( ":/main/sign_ok.svg" );
+	status_pixmap.insert( BackupTask::OK, QPixmap::fromImage( img ) );
+	img.load( ":/main/sign_error.svg" );
+	status_pixmap.insert( BackupTask::ERROR, QPixmap::fromImage( img ) );
+	img.load( ":/main/sign_warning.svg" );
+	status_pixmap.insert( BackupTask::WARNING, QPixmap::fromImage( img ) );
+	QList<BackupTask> lastBackups = settings->getLastBackups();
+	int n = std::min( settings->getNOfLastBackups(), lastBackups.size() );
 	// fill status-content of last backups into form
-	for (int i = 0; i < MAX_LAST_BACKUP_INFOS; i++)
+	for ( int i = 0; i < MAX_LAST_BACKUP_INFOS; i++ )
 	{
-		ScheduledTask::StatusEnum status = (ScheduledTask::StatusEnum)(i%3); // this line will be replaced by the array of last backups
-		bool isHidden = i >= n;
-		QLabel* lab_icon = this->findChild<QLabel *>(fieldnames_status[0].arg(i));
-		QLabel* lab_status = this->findChild<QLabel *>(fieldnames_status[1].arg(i));
-		QLabel* lab_date = this->findChild<QLabel *>(fieldnames_status[2].arg(i));
-		lab_icon->setHidden(isHidden);
-		lab_icon->setPixmap(status_pixmap.value(status));
-		lab_status->setHidden(isHidden);
-		lab_status->setText(ScheduledTask::getStatusText(status));
-		lab_date->setHidden(isHidden);
-		lab_date->setText((QTime::currentTime()).toString());
+		bool isHidden = (i >= n);
+		QLabel* lab_icon = this->findChild<QLabel *>( fieldnames_status[0].arg( i ) );
+		lab_icon->setHidden( isHidden );
+		QLabel* lab_status = this->findChild<QLabel *>( fieldnames_status[1].arg( i ) );
+		lab_status->setHidden( isHidden );
+		QLabel* lab_date = this->findChild<QLabel *>( fieldnames_status[2].arg( i ) );
+		lab_date->setHidden( isHidden );
+		if ( !isHidden )
+		{
+			BackupTask lastBackup = lastBackups.at( i );
+			BackupTask::StatusEnum status = lastBackup.getStatus();
+			lab_icon->setPixmap( status_pixmap.value( status ) );
+			lab_status->setText( lastBackup.getStatusText() );
+			lab_date->setText( lastBackup.getDateTime().toString() );
+		}
 	}
 	// fill information on next scheduled backup into form
-	if (myTask.getType() != ScheduledTask::NEVER) {
-		this->labelNextBackup->setText(QObject::tr("Scheduled"));
-		this->labelDateNextBackup->setText(this->myTask.toString());
-	} else {
- 		this->labelNextBackup->setText(QObject::tr("Not scheduled"));
-		this->labelDateNextBackup->setText("");
+	if ( myTask.getType() != ScheduledTask::NEVER )
+	{
+		this->labelNextBackup->setText( QObject::tr( "Scheduled" ) );
+		this->labelDateNextBackup->setText( this->myTask.toString() );
+	}
+	else
+	{
+		this->labelNextBackup->setText( QObject::tr( "Not scheduled" ) );
+		this->labelDateNextBackup->setText( "" );
 	}
 }
 
@@ -115,35 +126,35 @@ void OverviewForm::refreshSpaceStatistic()
 {
 	/** TODO: get quota from Mainmodel */
 	int quota = 5120, backup = 2403, snapshot = 1830;
-	this->labelSpaceAvailable->setPixmap(this->getSpaceVisualization(quota, backup, snapshot));
+	this->labelSpaceAvailable->setPixmap( this->getSpaceVisualization( quota, backup, snapshot ) );
 	// The following 3 lines could also be in the constructor
-	this->labelLegendBackup->setPixmap(this->getSpaceVisualization(100, 100, 0, 16, 1));
-	this->labelLegendSnapshot->setPixmap(this->getSpaceVisualization(100, 0, 100, 16, 1));
-	this->labelLegendFree->setPixmap(this->getSpaceVisualization(100, 0, 0, 16, 1));
+	this->labelLegendBackup->setPixmap( this->getSpaceVisualization( 100, 100, 0, 16, 1 ) );
+	this->labelLegendSnapshot->setPixmap( this->getSpaceVisualization( 100, 0, 100, 16, 1 ) );
+	this->labelLegendFree->setPixmap( this->getSpaceVisualization( 100, 0, 0, 16, 1 ) );
 
 	// definition of formats for output
-	QString format_percent = QString("%1\%"), format_MB = QString("%1 MB"), format_GB = QString("%1 GB"), format_TB = QString("%1 TB");
+	QString format_percent = QString( "%1\%" ), format_MB = QString( "%1 MB" ), format_GB = QString( "%1 GB" ), format_TB = QString( "%1 TB" );
 	int precision_MB = 0, precision_GB = 1, precision_TB = 2;
 	QStringList sizeNames; sizeNames << "Backup" << "Snapshot" << "Free" << "Quota";
-	QList<float> sizes; sizes << backup << snapshot << (quota - backup - snapshot) << quota;
+	QList<float> sizes; sizes << backup << snapshot << ( quota - backup - snapshot ) << quota;
 
 	// set field-text of all space-fields
-	for (int i = 0; i < sizeNames.size(); i++)
+	for ( int i = 0; i < sizeNames.size(); i++ )
 	{
-		QLabel * el_abs = this->findChild<QLabel *>(QString("labelSpace") + sizeNames[i]);
-		QLabel * el_percent = this->findChild<QLabel *>(QString("labelSpacePercent") + sizeNames[i]);
-		if (el_abs != 0)
+		QLabel * el_abs = this->findChild<QLabel *>( QString( "labelSpace" ) + sizeNames[i] );
+		QLabel * el_percent = this->findChild<QLabel *>( QString( "labelSpacePercent" ) + sizeNames[i] );
+		if ( el_abs != 0 )
 		{
-			if (sizes[i] >= 1024 * 1024)
-				el_abs->setText(format_TB.arg(sizes[i] / 1024 / 1024, 0, 'f', precision_TB));
-			else if (sizes[i] >= 1024)
-				el_abs->setText(format_GB.arg(sizes[i] / 1024, 0, 'f', precision_GB));
+			if ( sizes[i] >= 1024 * 1024 )
+				el_abs->setText( format_TB.arg( sizes[i] / 1024 / 1024, 0, 'f', precision_TB ) );
+			else if ( sizes[i] >= 1024 )
+				el_abs->setText( format_GB.arg( sizes[i] / 1024, 0, 'f', precision_GB ) );
 			else
-				el_abs->setText(format_MB.arg(sizes[i], 0, 'f', precision_MB));
+				el_abs->setText( format_MB.arg( sizes[i], 0, 'f', precision_MB ) );
 		}
-		if (el_percent != 0)
+		if ( el_percent != 0 )
 		{
-			el_percent->setText(format_percent.arg(100.0f*sizes[i] / sizes[sizes.size()-1], 0, 'f', 1));
+			el_percent->setText( format_percent.arg( 100.0f*sizes[i] / sizes[sizes.size()-1], 0, 'f', 1 ) );
 		}
 	}
 }
@@ -151,43 +162,43 @@ void OverviewForm::refreshSpaceStatistic()
 /**
 * returns a QPixmap-bar, showing used, snapshot and free space in different colors
 */
-QPixmap OverviewForm::getSpaceVisualization(int quota, int used, int snapshot, int imgH, int imgW)
+QPixmap OverviewForm::getSpaceVisualization( int quota, int used, int snapshot, int imgH, int imgW )
 {
-	QImage spaceImg = QImage(imgW, imgH, QImage::Format_RGB32);
-	spaceImg.fill(COLOR_WHITE);
+	QImage spaceImg = QImage( imgW, imgH, QImage::Format_RGB32 );
+	spaceImg.fill( COLOR_WHITE );
 	float thresholdX = 0.5f; // brightness at which original color is left untouched, below darkend, above brightend
 	int i = 0;
-	for (int j = 0; j < imgH; j++)
+	for ( int j = 0; j < imgH; j++ )
 	{
-		QRgb* rgbData = (QRgb*)(spaceImg.scanLine(j));
-		float b = (float)pow(sin((75 + (j / (imgH - 1.0)) * 110.0) / 180.0 * 3.141592), 2.0) * 0.7f + 0.2;
-		QRgb c = linIP(COLOR_BLACK, COLOR_BACKUP, COLOR_WHITE, thresholdX, b);
-		for (i = 0; i < floor((float)imgW*used / quota); i++)
+		QRgb* rgbData = ( QRgb* )( spaceImg.scanLine( j ) );
+		float b = ( float )pow( sin(( 75 + ( j / ( imgH - 1.0 ) ) * 110.0 ) / 180.0 * 3.141592 ), 2.0 ) * 0.7f + 0.2;
+		QRgb c = linIP( COLOR_BLACK, COLOR_BACKUP, COLOR_WHITE, thresholdX, b );
+		for ( i = 0; i < floor(( float )imgW*used / quota ); i++ )
 			rgbData[i] = c;
-		c = linIP(COLOR_BLACK, COLOR_SNAPSHOT, COLOR_WHITE, thresholdX, b);
-		for (;i < floor((float)imgW*(used + snapshot) / quota);i++)
+		c = linIP( COLOR_BLACK, COLOR_SNAPSHOT, COLOR_WHITE, thresholdX, b );
+		for ( ;i < floor(( float )imgW*( used + snapshot ) / quota );i++ )
 			rgbData[i] = c;
-		c = linIP(COLOR_BLACK, COLOR_FREE, COLOR_WHITE, thresholdX, b);
-		for (;i < imgW;i++)
+		c = linIP( COLOR_BLACK, COLOR_FREE, COLOR_WHITE, thresholdX, b );
+		for ( ;i < imgW;i++ )
 			rgbData[i] = c;
 	}
-	return QPixmap::fromImage(spaceImg);
+	return QPixmap::fromImage( spaceImg );
 }
 
-QRgb OverviewForm::linIP(QRgb v0, QRgb vMid, QRgb v1, float mid, float x)
+QRgb OverviewForm::linIP( QRgb v0, QRgb vMid, QRgb v1, float mid, float x )
 {
 	float x0, x1;
 	QColor y0, y1;
-	if (x <= mid)
+	if ( x <= mid )
 	{
-		x0 = 0; x1 = mid; y0 = QColor(v0); y1 = QColor(vMid);
+		x0 = 0; x1 = mid; y0 = QColor( v0 ); y1 = QColor( vMid );
 	}
 	else
 	{
-		x0 = mid; x1 = 1; y0 = QColor(vMid); y1 = QColor(v1);
+		x0 = mid; x1 = 1; y0 = QColor( vMid ); y1 = QColor( v1 );
 	}
-	float q0 = (x - x0) / (x1 - x0);
-	float q1 = (x1 - x) / (x1 - x0);
-	return qRgba((int)(q0*y1.red() + q1*y0.red()), (int)(q0*y1.green() + q1*y0.green()), (int)(q0*y1.blue() + q1*y0.blue()), (int)(q0*y1.alpha() + q1*y0.alpha()));
+	float q0 = ( x - x0 ) / ( x1 - x0 );
+	float q1 = ( x1 - x ) / ( x1 - x0 );
+	return qRgba(( int )( q0*y1.red() + q1*y0.red() ), ( int )( q0*y1.green() + q1*y0.green() ), ( int )( q0*y1.blue() + q1*y0.blue() ), ( int )( q0*y1.alpha() + q1*y0.alpha() ) );
 }
 
