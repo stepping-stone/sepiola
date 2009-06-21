100b5fbed61e99c3f83773949a68acb638a197aa
Dominic Sydler <sydler@puzzle.ch>
2009-06-21 05:26:54 +0000

    problem on build system using std::min, trying to use namespace std.

--- src/utils/string_utils.hh
+++ src/utils/string_utils.hh
@@ -22,7 +22,7 @@
 #include <QString>
 #include <QDebug>
 #include <math.h>
-#include <ctype.h>
+using namespace std;
 
 typedef QList<QPair<QString,QString> > StringPairList;
 Q_DECLARE_METATYPE( StringPairList );
@@ -154,7 +154,7 @@ inline QString StringUtils::kBytesToReadableStr(double traffic, const QString& u
 }
 
 inline QString StringUtils::equalStart(const QString& aStr, const QString& bStr) {
-	int i = 0, imax = std::min(aStr.length(),bStr.length());
+	int i = 0, imax = ::min(aStr.length(),bStr.length());
 	while (i < imax && aStr[i]==bStr[i]) i++;
 	return aStr.left(i);
 }
