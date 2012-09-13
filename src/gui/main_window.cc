7486e120e27554e3598d022a81b050da0a27642d
Tiziano Müller <tiziano.mueller@stepping-stone.ch>
2012-09-13 16:22:03 +0000

    Copyright and header fix.
    * Add missing headers
    * Fix incorrect headers
    * Update copyright date to reflect latest change

--- src/gui/main_window.cc
+++ src/gui/main_window.cc
@@ -1,6 +1,6 @@
 /*
 #| sepiola - Open Source Online Backup Client
-#| Copyright (C) 2007, 2008 stepping stone GmbH
+#| Copyright (C) 2007-2011 stepping stone GmbH
 #|
 #| This program is free software; you can redistribute it and/or
 #| modify it under the terms of the GNU General Public License
@@ -8,12 +8,12 @@
 #|
 #| This program is distributed in the hope that it will be useful,
 #| but WITHOUT ANY WARRANTY; without even the implied warranty of
-#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #| GNU General Public License for more details.
 #|
 #| You should have received a copy of the GNU General Public License
 #| along with this program; if not, write to the Free Software
-#| Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#| Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 #include <QDebug>
--- src/test/test_host_file_utils.cc
+++ src/test/test_host_file_utils.cc
@@ -1,11 +1,20 @@
-/* vim: set sw=4 sts=4 ft=cpp et foldmethod=syntax : */
-
 /*
- * Copyright (c) 2012 Tiziano Müller <tm@dev-zero.ch>
- *
- *
- *
- */
+#| sepiola - Open Source Online Backup Client
+#| Copyright (C) 2012 stepping stone GmbH
+#|
+#| This program is free software; you can redistribute it and/or
+#| modify it under the terms of the GNU General Public License
+#| Version 2 as published by the Free Software Foundation.
+#|
+#| This program is distributed in the hope that it will be useful,
+#| but WITHOUT ANY WARRANTY; without even the implied warranty of
+#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#| GNU General Public License for more details.
+#|
+#| You should have received a copy of the GNU General Public License
+#| along with this program; if not, write to the Free Software
+#| Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 #include "test_host_file_utils.hh"
 
