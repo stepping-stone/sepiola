d4bf85271e48b027ef113db110396546f45be5f4
Tiziano Müller <tm@dev-zero.ch>
2013-08-17 00:24:43 +0200

    Add class for displaying stacked bars
    This is an AbstractItemView-implementation.
    Based losely on the AbstractItemView-Qt-Demo-Application.

---
+++ src/gui/stacked_bar_view.cc
@@ -0,0 +1,227 @@
+/* vim: set sw=4 sts=4 ft=cpp et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2011 Tiziano Müller <tm@dev-zero.ch>
+ *
+ *
+ *
+ */
+
+#include "stacked_bar_view.hh"
+
+#include <QtGui/QPen>
+#include <QtGui/QPainter>
+#include <QtGui/QPaintEvent>
+#include <QtGui/QScrollBar>
+
+namespace
+{
+    static const int DATA_COLUMN = 0;
+    static const int COLOR_COLUMN = 0;
+}
+
+StackedBarView::StackedBarView(QWidget* p) :
+    QAbstractItemView(p),
+    _margin(0),
+    _stackbarHeight(16),
+    _totalValue(0.0)
+{
+}
+
+QModelIndex	StackedBarView::indexAt(const QPoint& point) const
+{
+    if (_validItems == 0)
+        return QModelIndex();
+
+    // Transform the view coordinates into contents widget coordinates.
+    int wx = point.x() + horizontalScrollBar()->value() - _margin;
+    int wy = point.y() + verticalScrollBar()->value() - _margin;
+
+    if ((wx < 0) || (wx > size().width()-_margin) || (wy < 0) || (wy > size().width()-_margin))
+        return QModelIndex();
+
+    double start(0.);
+
+    for (int row(start), end(model()->rowCount(rootIndex())); row <= end; ++row)
+    {
+        const QModelIndex valueIndex(model()->index(row, DATA_COLUMN, rootIndex()));
+        const double value(model()->data(valueIndex).toDouble());
+        const QString color(model()->data(model()->index(row, COLOR_COLUMN, rootIndex()), Qt::DecorationRole).toString());
+
+        if (value > 0.0 && !color.isNull())
+        {
+            start += (size().width()-2.*_margin) * (value/_totalValue);
+            if (point.x() < start)
+                return valueIndex;
+        }
+    }
+
+    return QModelIndex();
+}
+
+void StackedBarView::scrollTo(const QModelIndex&, ScrollHint)
+{
+}
+
+QRect StackedBarView::visualRect(const QModelIndex&) const
+{
+    return QRect();
+}
+
+int	StackedBarView::horizontalOffset() const
+{
+    return horizontalScrollBar()->value();
+}
+
+int	StackedBarView::verticalOffset() const
+{
+    return verticalScrollBar()->value();
+}
+
+bool StackedBarView::isIndexHidden(const QModelIndex& index) const
+{
+    const QString color(model()->data(model()->index(index.row(), COLOR_COLUMN, rootIndex()), Qt::DecorationRole).toString());
+    const double value(model()->data(model()->index(index.row(), DATA_COLUMN, rootIndex())).toDouble());
+    return (value <= 0.0 || color.isNull());
+}
+
+QModelIndex	StackedBarView::moveCursor(CursorAction, Qt::KeyboardModifiers)
+{
+    return QModelIndex();
+}
+
+void StackedBarView::setSelection(const QRect &, QItemSelectionModel::SelectionFlags)
+{
+}
+
+QRegion	StackedBarView::visualRegionForSelection(const QItemSelection &) const
+{
+    return QRegion();
+}
+
+void StackedBarView::paintEvent(QPaintEvent* event)
+{
+    QStyleOptionViewItem option(viewOptions());
+
+    QBrush background(option.palette.color(QPalette::Window));
+    QPen foreground(option.palette.color(QPalette::WindowText));
+
+    QPainter painter(viewport());
+    painter.setRenderHint(QPainter::Antialiasing);
+
+    painter.fillRect(event->rect(), background);
+    painter.setPen(foreground);
+
+    painter.save();
+
+    const double stackbarTotalLength(size().width()-2.*_margin);
+    const QRect barRect(_margin, _margin, stackbarTotalLength, _stackbarHeight);
+
+    // Viewport rectangles
+    painter.translate(barRect.x() - horizontalScrollBar()->value(),
+                      barRect.y() - verticalScrollBar()->value());
+    painter.drawRect(0, 0, stackbarTotalLength, _stackbarHeight);
+    painter.setPen(Qt::NoPen);
+
+    QLinearGradient gradient( QPoint(0, 0), QPoint(0, _stackbarHeight));
+    gradient.setColorAt(0, Qt::white);
+    gradient.setColorAt(1, Qt::black);
+
+    if (_validItems > 0)
+    {
+        double start(0.);
+        QAbstractItemModel * m(model());
+
+        for (int row(0), end(m->rowCount(rootIndex())); row <= end; ++row)
+        {
+            const double value(m->data(m->index(row, DATA_COLUMN, rootIndex())).toDouble());
+            const QString color(m->data(m->index(row, COLOR_COLUMN, rootIndex()), Qt::DecorationRole).toString());
+
+            if (value > 0.0 && !color.isNull())
+            {
+                const double length(stackbarTotalLength * (value/_totalValue));
+                gradient.setColorAt(0.5, color);
+                painter.setBrush(gradient);
+                painter.drawRect(QRectF(start, 0., length, _stackbarHeight));
+                start += length;
+            }
+        }
+    }
+    else
+    {
+        // draw a black background bar
+        gradient.setColorAt(0.5, Qt::black);
+        painter.setBrush(gradient);
+        painter.drawRect(QRectF(0., 0., stackbarTotalLength, _stackbarHeight));
+    }
+
+    painter.restore();
+}
+
+void StackedBarView::dataChanged(const QModelIndex &topLeft, const QModelIndex &bottomRight)
+{
+    QAbstractItemView::dataChanged(topLeft, bottomRight);
+
+    _validItems = 0;
+    _totalValue = 0.0;
+
+    QAbstractItemModel * m(model());
+
+    for (int row(0), end(m->rowCount(rootIndex())); row <= end; ++row)
+    {
+        double value(m->data(m->index(row, DATA_COLUMN, rootIndex())).toDouble());
+        QString color(m->data(m->index(row, COLOR_COLUMN, rootIndex()), Qt::DecorationRole).toString());
+
+        if (value > 0.0 && !color.isNull())
+        {
+            _validItems += 1;
+            _totalValue += value;
+        }    
+    }
+
+    viewport()->update();
+}
+
+void StackedBarView::rowsInserted(const QModelIndex &parent, int start, int end)
+{
+    QAbstractItemModel * m(model());
+
+    for (int row(start); row <= end; ++row)
+    {
+        const double value(m->data(m->index(row, DATA_COLUMN, rootIndex())).toDouble());
+        const QString color(m->data(m->index(row, COLOR_COLUMN, rootIndex()), Qt::DecorationRole).toString());
+
+        if (value > 0.0 && !color.isNull())
+        {
+            _validItems += 1;
+            _totalValue += value;
+        }    
+    }
+
+    QAbstractItemView::rowsInserted(parent, start, end);
+}
+
+void StackedBarView::rowsAboutToBeRemoved(const QModelIndex &parent, int start, int end)
+{
+    QAbstractItemModel * m(model());
+
+    for (int row(start); row <= end; ++row)
+    {
+        const double value(m->data(m->index(row, DATA_COLUMN, rootIndex())).toDouble());
+        const QString color(m->data(m->index(row, COLOR_COLUMN, rootIndex()), Qt::DecorationRole).toString());
+
+        if (value > 0.0 && !color.isNull())
+        {
+            _validItems -= 1;
+            _totalValue -= value;
+        }    
+    }
+
+    QAbstractItemView::rowsAboutToBeRemoved(parent, start, end);
+}
+
+bool StackedBarView::edit(const QModelIndex&, EditTrigger, QEvent*)
+{
+    // disable editing for all
+    return false;
+}
---
+++ src/gui/stacked_bar_view.hh
@@ -0,0 +1,58 @@
+/* vim: set sw=4 sts=4 ft=cpp et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2011 Tiziano Müller <tm@dev-zero.ch>
+ *
+ *
+ *
+ */
+
+#ifndef STACKED_BAR_VIEW_HH
+#define STACKED_BAR_VIEW_HH
+
+#include <QtGui/QAbstractItemView>
+
+class StackedBarView :
+    public QAbstractItemView
+{
+    Q_OBJECT
+
+public:
+    StackedBarView(QWidget* p = 0);
+
+    QModelIndex	indexAt(const QPoint & point) const;
+
+    void scrollTo(const QModelIndex & index, ScrollHint hint = EnsureVisible);
+
+    QRect visualRect(const QModelIndex & index) const;
+
+protected:
+    int	horizontalOffset() const;
+    int	verticalOffset() const;
+
+    bool isIndexHidden(const QModelIndex & index) const;
+
+    QModelIndex	moveCursor(CursorAction cursorAction, Qt::KeyboardModifiers modifiers);
+
+    void setSelection(const QRect & rect, QItemSelectionModel::SelectionFlags flags);
+
+    QRegion	visualRegionForSelection(const QItemSelection & selection) const;
+
+    void paintEvent(QPaintEvent* event);
+
+    bool edit (const QModelIndex&, EditTrigger, QEvent*);
+
+protected slots:
+    void dataChanged(const QModelIndex &topLeft, const QModelIndex &bottomRight);
+    void rowsInserted(const QModelIndex &parent, int start, int end);
+    void rowsAboutToBeRemoved(const QModelIndex &parent, int start, int end);
+
+private:
+    void _updateColorValues();
+
+    int _margin, _totalSize, _stackbarHeight;
+    double _totalValue;
+    size_t _validItems;
+};
+
+#endif
