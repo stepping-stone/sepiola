f0ca1aacd40c44d9f8b11fa14288d51af218570b
patklaey <pat.klaey@stepping-stone.ch>
2014-04-28 14:32:53 +0200

    Implementing DummySnapshot class

---
+++ src/tools/dummy_snapshot.cc
@@ -0,0 +1,49 @@
+/*
+#| sepiola - Open Source Online Backup Client
+#| Copyright (C) 2007-2014 stepping stone GmbH
+#|
+#| This program is free software; you can redistribute it and/or
+#| modify it under the terms of the GNU General Public License
+#| Version 2 as published by the Free Software Foundation.
+#|
+#| This program is distributed in the hope that it will be useful,
+#| but WITHOUT ANY WARRANTY; without even the implied warranty of
+#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#| GNU General Public License for more details.
+#|
+#| You should have received a copy of the GNU General Public License
+#| along with this program; if not, write to the Free Software
+#| Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include "dummy_snapshot.hh"
+
+DummySnapshot::DummySnapshot()
+{
+    // Empty constructor, just send the signal that the object has been
+    // created
+    emit sendSnapshotObjectCreated( SNAPSHOT_SUCCESS );
+}
+
+DummySnapshot::~DummySnapshot()
+{
+    // As the constructor is empty, the destructor can be empty too
+}
+
+void DummySnapshot::initializeSnapshot()
+{
+    // Simply send the snapshot initialized signal (do nothing at all)
+    emit sendSnapshotInitialized( SNAPSHOT_SUCCESS );
+}
+
+void DummySnapshot::addFilesToSnapshot( BackupSelectionHash& includeRules)
+{
+    // Again nothing to do here, just send the signal that the files were added
+    emit sendFilesAddedToSnapshot( SNAPSHOT_SUCCESS );
+}
+
+void DummySnapshot::takeSnapshot()
+{
+    // Again, no action needed, just send the signal that the snapshot is taken
+    emit sendSnapshotTaken( SNAPSHOT_SUCCESS );
+}
---
+++ src/tools/dummy_snapshot.hh
@@ -0,0 +1,43 @@
+/*
+#| sepiola - Open Source Online Backup Client
+#| Copyright (C) 2007-2014 stepping stone GmbH
+#|
+#| This program is free software; you can redistribute it and/or
+#| modify it under the terms of the GNU General Public License
+#| Version 2 as published by the Free Software Foundation.
+#|
+#| This program is distributed in the hope that it will be useful,
+#| but WITHOUT ANY WARRANTY; without even the implied warranty of
+#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#| GNU General Public License for more details.
+#|
+#| You should have received a copy of the GNU General Public License
+#| along with this program; if not, write to the Free Software
+#| Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef DUMMY_SNAPSHOT_HH_
+#define DUMMY_SNAPSHOT_HH_
+
+#include "abstract_snapshot.hh"
+
+/**
+ * The dummy snapshot class provides an empty snapshot method
+ * @author Pat Kläy, pat.klaey@stepping-stone.ch
+ */
+class DummySnapshot: public AbstractSnapshot
+{
+public:
+
+    /**
+     * Creates the DummySnapshot object
+     */
+    DummySnapshot();
+
+    /**
+     * Destroys the DummySnapshot
+     */
+    virtual ~DummySnapshot();
+};
+
+#endif /* DUMMY_SNAPSHOT_HH_ */
