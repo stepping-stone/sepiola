89c73793c49b182200b06c541d53d50429256d68
patklaey <pat.klaey@stepping-stone.ch>
2014-04-30 10:49:16 +0200

    Implementing new FilesystemSnapshotPathMapper class

---
+++ src/tools/filesystem_snapshot_path_mapper.cc
@@ -0,0 +1,50 @@
+/*
+ * filesystem_snapshot_path_mapper.cc
+ *
+ *  Created on: Apr 30, 2014
+ *      Author: pat
+ */
+
+#include "filesystem_snapshot_path_mapper.hh"
+
+#include <QString>
+
+FilesystemSnapshotPathMapper::FilesystemSnapshotPathMapper(const QString& _partition, const BackupSelectionHash& includeRules )
+{
+    this->partition = _partition;
+    this->relativeIncludes = includeRules;
+}
+
+FilesystemSnapshotPathMapper::~FilesystemSnapshotPathMapper()
+{
+}
+
+const QString& FilesystemSnapshotPathMapper::getPartition() const
+{
+    return partition;
+}
+
+void FilesystemSnapshotPathMapper::setPartition(const QString& partition)
+{
+    this->partition = partition;
+}
+
+const BackupSelectionHash& FilesystemSnapshotPathMapper::getRelativeIncludes() const
+{
+    return relativeIncludes;
+}
+
+void FilesystemSnapshotPathMapper::setRelativeIncludes(const BackupSelectionHash& relativeIncludes)
+{
+    this->relativeIncludes = relativeIncludes;
+}
+
+const QString& FilesystemSnapshotPathMapper::getSnapshotPath() const
+{
+    return snapshotPath;
+}
+
+void FilesystemSnapshotPathMapper::setSnapshotPath(const QString& snapshotPath)
+{
+    this->snapshotPath = snapshotPath;
+}
---
+++ src/tools/filesystem_snapshot_path_mapper.hh
@@ -0,0 +1,87 @@
+/*
+#| sepiola - Open Source Online Backup Client
+#| Copyright (C) 2007-2014 stepping stone GmbH
+#|
+#| This program is free software; you can redistribute it and/or
+#| modify it under the terms of the GNU General Public License
+#| Version 2 as published by the Free Software Foundation.
+#|
+#| This program is distributed in the hope that it will be useful,
+#| but WITHOUT ANY WARRANTY; without even the implied warranty of
+#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#| GNU General Public License for more details.
+#|
+#| You should have received a copy of the GNU General Public License
+#| along with this program; if not, write to the Free Software
+#| Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef FILESYSTEM_SNAPSHOT_PATH_MAPPER_HH_
+#define FILESYSTEM_SNAPSHOT_PATH_MAPPER_HH_
+
+#include "utils/datatypes.hh"
+
+class QString;
+
+class FilesystemSnapshotPathMapper
+{
+public:
+
+    /**
+     * Constructs the FilesystemSnapshotPathMapper
+     * @param The partition (mount point)
+     * @param A BackupSelectionHash which contains all files (with relative file
+     * path) on this partition
+     */
+    FilesystemSnapshotPathMapper(const QString& _partition, const BackupSelectionHash& includeRules);
+
+    /**
+     * Destroys the FilesystemSnapshotPathMapper
+     */
+    virtual ~FilesystemSnapshotPathMapper();
+
+    // Getters
+    /**
+     * Returns the partition name (mount point)
+     * @return The partition name (mount point)
+     */
+    const QString& getPartition() const;
+
+    /**
+     * Returns all files on the given partition
+     * @return All files on the given partition
+     */
+    const BackupSelectionHash& getRelativeIncludes() const;
+
+    /**
+     * Returns the local snapshot path for the given partition
+     * @return The local snapshot path for the given partition
+     */
+    const QString& getSnapshotPath() const;
+
+    // Setters
+    /**
+     * Sets the partition name (mount point)
+     * @param The partition name (mount point)
+     */
+    void setPartition(const QString& partition);
+
+    /**
+     * Sets the relative include rules (for the given partition)
+     * @param The relative BackupSelectionHash
+     */
+    void setRelativeIncludes(const BackupSelectionHash& relativeIncludes);
+
+    /**
+     * Sets the local snapshot path for the given partition
+     * @param The local snapshot path for the given partition
+     */
+    void setSnapshotPath(const QString& snapshotPath);
+
+private:
+    QString partition;
+    QString snapshotPath;
+    BackupSelectionHash relativeIncludes;
+};
+
+#endif /* FILESYSTEM_SNAPSHOT_PATH_MAPPER_HH_ */
