21200530984f0c531e9cd47717768360247135d6
Pascal Jufer <pascal.jufer@stepping-stone.ch>
2020-06-29 19:16:39 +0200

    Add working console snippet for Windows
    Commented out by default

--- src/main.cc
+++ src/main.cc
@@ -207,8 +207,27 @@ void createConsole()
 }
 #endif
 
+// Console for debugging purposes on Windows
+// (you need to call 'Console();' in main function)
+#if 0
+void Console()
+{
+	AllocConsole();
+	FILE *pFileCon = NULL;
+	pFileCon = freopen("CONOUT$", "w", stdout);
+
+	COORD coordInfo;
+	coordInfo.X = 130;
+	coordInfo.Y = 9000;
+
+	SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), coordInfo);
+	SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE),ENABLE_QUICK_EDIT_MODE| ENABLE_EXTENDED_FLAGS);
+}
+#endif
+
 int main(int argc, char *argv[])
 {
+	//Console();
 	setHomeDir();
 
 	if (CliManager::isNoopApplication(argc, argv) )
